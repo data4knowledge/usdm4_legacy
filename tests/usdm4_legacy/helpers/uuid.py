a992ebdef724b06b389d3c3eab2a638b6a2ad67c
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2025-08-15 07:56:50 +0200

    Update tests, add more integration and organise

---
+++ tests/usdm4_legacy/helpers/uuid.py
@@ -0,0 +1,8 @@
+import re
+
+def replace_uuid(result):
+    return re.sub(
+        r"[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}",
+        "FAKE",
+        result,
+    )
--- tests/usdm4_legacy/test_integration.py
+++ tests/usdm4_legacy/test_integration.py
@@ -1,7 +1,7 @@
-import re
 import json
-from tests.usdm4_legacy.errors.errors_clean import errors_clean_all
-from tests.usdm4_legacy.files.files import read_json, write_json, write_yaml, read_yaml
+from tests.usdm4_legacy.helpers.uuid import replace_uuid
+from tests.usdm4_legacy.helpers.errors_clean import errors_clean_all
+from tests.usdm4_legacy.helpers.files import read_json, write_json, write_yaml, read_yaml
 from src.usdm4_legacy import USDM4Legacy
 from usdm4.api.wrapper import Wrapper
 
@@ -12,15 +12,13 @@ SAVE = True
 #     return "asyncio"
 
 
-def _run_test(dir, name):
+def _run_test(name):
     filename = f"{name}.pdf"
-    filepath = _full_path(dir, filename)
+    filepath = _full_path(name, filename)
     legacy = USDM4Legacy()
     wrapper: Wrapper = legacy.from_pdf(filepath)
-    print(f"ERRORS: {legacy.errors.dump(0)}")
     result = wrapper.to_json()
     result = replace_uuid(result)
-    # print(f"MATCH: {result[10:70]}")
     pretty_result = json.dumps(json.loads(result), indent=2)
     result_filename = filename = f"{name}_usdm.json"
     error_filename = filename = f"{name}_errors.yaml"
@@ -34,17 +32,16 @@ def _run_test(dir, name):
 
 
 def _full_path(dir, filename):
-    return f"tests/usdm4_legacy/test_files/{dir}/{filename}"
+    return f"tests/usdm4_legacy/test_files/protocols/{dir}/{filename}"
 
+def test_nct01847274():
+    _run_test("NCT01847274")
 
-#@pytest.mark.anyio
+def test_nct03637764():
+    _run_test("NCT03637764")
+
 def test_nct04184622():
-    _run_test("protocols", "NCT04184622")
+    _run_test("NCT04184622")
 
-
-def replace_uuid(result):
-    return re.sub(
-        r"[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}",
-        "FAKE",
-        result,
-    )
+def test_nct04320615():
+    _run_test("protocols/NCT04320615", "NCT04320615")
